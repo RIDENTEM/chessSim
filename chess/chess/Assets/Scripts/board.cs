ddf0b3e7cde009d82293b1b1647d439455c0b97c
Jonathan Ribarro <Jcribarro@gmail.com>
2018-11-18 23:51:26 -0500

    Got rid of most of the code I had before, if I am doing this it has to be by myself, and also built the board and multi colored tiles, right now they dont do anything

---
+++ chess/chess/Assets/Scripts/board.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class board : MonoBehaviour
+{
+
+    [SerializeField] GameObject tile;
+
+    void generateBoard()
+    {
+        float xPos = -3.0f;
+        float yPos = 3.0f;
+        Vector3 whereToSpawn;
+        bool opp = false;
+        for (int i = 0; i < 8; i++, yPos -= 0.5f)
+        {
+            for (int j = 0; j < 8; j++, xPos += 0.5f)
+            {
+                if (opp == false)
+                {
+                    if (j % 2 == 0)
+                    {
+                        tile.GetComponent<SpriteRenderer>().color = Color.white;
+                    }
+                    else
+                        tile.GetComponent<SpriteRenderer>().color = Color.black;
+                }
+                else
+                {
+                    if (j % 2 == 0)
+                    {
+                        tile.GetComponent<SpriteRenderer>().color = Color.black;
+                    }
+                    else
+                        tile.GetComponent<SpriteRenderer>().color = Color.white;
+
+
+                }
+
+                whereToSpawn = new Vector3(xPos, yPos, 0.0f);
+                Instantiate(tile, whereToSpawn, Quaternion.identity);
+            }
+            xPos = -3.0f;
+            if (opp == false)
+                opp = true;
+            else
+                opp = false;
+
+        }
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        generateBoard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ chess/chess/Assets/Scripts/tile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tile : MonoBehaviour
+{
+
+    Collider2D tileCollider;
+     
+
+    void onTileClick()
+    {
+      //  this.gameObject.GetComponent<Sprite>().texture.
+    }
+
+    private void OnGUI()
+    {
+        
+    } 
+
+}
